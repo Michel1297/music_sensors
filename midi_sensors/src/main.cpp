f5069ae1d736bf2a1f4d0801a7e724ad12ca63eb
Pelela <mvrozas@uc.cl>
2023-02-02 22:32:02 -0300

    minor changes

--- midi_sensors/src/main.cpp
+++ midi_sensors/src/main.cpp
@@ -8,6 +8,10 @@
 TaskHandle_t Task1; //Task encargado de leer los sensores
 TaskHandle_t Task2; //Task encargado de enviar las notas via MIDI BLE
 
+//Creamos la instancia de la IMU MPU6050
+Adafruit_MPU6050 mpu;
+sensors_event_t a, g, temp;
+
 //Definición de variables y parámetros
 
 #define encoder0PinA  34      //Pin del encoder del caudalómetro
@@ -25,10 +29,10 @@ void doEncoder0A(){
 int ObtenerFrecuencia() 
 {
   int frecuencia;
-  NumPulsos = 0;   //Ponemos a 0 el número de pulsos
-  interrupts();    //Habilitamos las interrupciones
-  delay(100);   //muestra de 1 segundo
-  noInterrupts(); //Desabilitamos las interrupciones
+  NumPulsos = 0;           //Ponemos a 0 el número de pulsos
+  interrupts();            //Habilitamos las interrupciones
+  delay(50);               //muestra de 1 segundo
+  noInterrupts();          //Desabilitamos las interrupciones
   frecuencia=NumPulsos*10; //Hz(pulsos por segundo)
   return frecuencia;
 }
@@ -39,11 +43,41 @@ void sensors_control (void *param){
   
   
   Serial.begin(115200);           //Inicializamos la comunicación Serial
-  Wire.begin(32,33,400000UL);     //Inicializamos la comunicación i2C para la IMU MPU6050
 
-  //Creamos la instancia de la IMU MPU6050
-  Adafruit_MPU6050 mpu;
-  sensors_event_t a, g, temp;
+  //Configuración de la interrupción que calcula el caudal a partir de los datos del encoder
+  pinMode(encoder0PinA, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoder0A, RISING);  // encoder PIN A
+  
+  while(true){
+    
+    //Datos de sensor de caudal 
+    float frecuencia=ObtenerFrecuencia();           //obtenemos la Frecuencia de los pulsos en Hz
+    float caudal_L_m = frecuencia/factor_conversion;  //calculamos el caudal en L/m
+    float caudal_L_h = caudal_L_m*60;                 //calculamos el caudal en L/h
+
+    
+  
+
+
+    d1 = map(frecuencia,0,300,50,80);
+
+    vTaskDelay(pdMS_TO_TICKS(5)); //Delay 1 ms
+  }
+
+  
+}
+
+void MIDI_control (void *param){
+  
+  //Imprimimos un mensjase de inicio de la conexión BLE MIDI
+  Serial.println("Inicializando BLE MIDI");
+  BLEMidiServer.begin("ESP32 MIDI SENSOR 1");
+  Serial.println("Esperando conexión con dispositivo");
+  
+    Wire.begin(32,33);     //Inicializamos la comunicación i2C para la IMU MPU6050
+
+
   //Variables locales de la IMU
   float gyroX, gyroY, gyroZ;
   float accX, accY, accZ;
@@ -56,25 +90,21 @@ void sensors_control (void *param){
   //Inicialación de la IMU
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
   }
   Serial.println("MPU6050 Found!");
 
-  //Configuración de la interrupción que calcula el caudal a partir de los datos del encoder
-  pinMode(encoder0PinA, INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoder0A, RISING);  // encoder PIN A
-  
+
   while(true){
-    
-    //Datos de sensor de caudal 
-    float frecuencia=ObtenerFrecuencia();          //obtenemos la Frecuencia de los pulsos en Hz
-    float caudal_L_m=frecuencia/factor_conversion; //calculamos el caudal en L/m
-    float caudal_L_h=caudal_L_m*60;                //calculamos el caudal en L/h
     
     //Datos MPU6050
     mpu.getEvent(&a, &g, &temp);
+
+    //Cargamos los datos actuales de los sensores de caudal e IMU en el Thread MIDI
+
+    //Datos de prueba random (también se pueden colocar otras entradas de datos)
+    //int note = random(50,80);
+    //uint8_t velocity = random(0,255);
+    //float pitch = random(0,127)/10.0;
 
     float gyroX_temp = g.gyro.x;
     if(abs(gyroX_temp) > gyroXerror)  {
@@ -90,32 +120,10 @@ void sensors_control (void *param){
     }
 
     //Asingación y mapeo de los datos obtenidos de la IMU y el Caudal
-    d1 = map(frecuencia,0,300,50,80);
+   
     d2 = (int)map(abs(gyroX*180*PI),0,360,0,127); if(d2>127){d2 = 127;}
     d3 = (int)map(abs(gyroY*180*PI),0,360,0,127); if(d3>127){d3 = 127;}
     d4 = (int)map(abs(gyroZ*180*PI),0,360,0,127); if(d4>127){d4 = 127;}
-    
-    vTaskDelay(pdMS_TO_TICKS(1)); //Delay 1 ms
-  }
-
-  
-}
-
-void MIDI_control (void *param){
-  
-  //Imprimimos un mensjase de inicio de la conexión BLE MIDI
-  Serial.println("Inicializando BLE MIDI");
-  BLEMidiServer.begin("ESP32 MIDI SENSOR 1");
-  Serial.println("Esperando conexión con dispositivo");
-
-  while(true){
-    
-    //Cargamos los datos actuales de los sensores de caudal e IMU en el Thread MIDI
-
-    //Datos de prueba random (también se pueden colocar otras entradas de datos)
-    //int note = random(50,80);
-    //uint8_t velocity = random(0,255);
-    //float pitch = random(0,127)/10.0;
 
     int note = d1;
     uint8_t velocity = d4;
@@ -143,18 +151,36 @@ void MIDI_control (void *param){
         vTaskDelay(pdMS_TO_TICKS(10));
       }
     }
+    else{
+      //Mostramos los valores enviados en la pantalla del computador
+      Serial.print("Note: ");
+      Serial.print(note);
+      Serial.print(", Velocity: ");
+      Serial.print(velocity);
+      Serial.print(", Pitchbend:");
+      Serial.print(pitch);
+      Serial.println();
+
+      vTaskDelay(pdMS_TO_TICKS(70));
+
+    }
+    vTaskDelay(pdMS_TO_TICKS(5));
   }
-  vTaskDelay(pdMS_TO_TICKS(1));
+  
 }
 
 void setup() {
   
   //Creamos los dos Threads principales
-  xTaskCreatePinnedToCore(sensors_control,"Task1",4096,NULL,1,&Task1,0); //Control y lectura de sensores, en el núcleo 0
-  xTaskCreatePinnedToCore(MIDI_control,"Task2",4096,NULL,1,&Task2,1);    //Comunicación MIDI, en el núcleo 1
+  //disableCore0WDT();
+
+  xTaskCreatePinnedToCore(sensors_control,"Task1",10000,NULL,1,&Task1,0); //Control y lectura de sensores, en el núcleo 0
+  xTaskCreatePinnedToCore(MIDI_control,"Task2",10000,NULL,1,&Task2,1);    //Comunicación MIDI, en el núcleo 1
 }
 
 void loop() {
   
 }
+
+
 
